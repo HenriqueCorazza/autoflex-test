83b36990c37188ce68f6cc6c26022eba5587230d
Henrique Corazza <henrique.corazza@hotmail.com>
2026-02-27 14:46:20 -0300

    feat: add ProductController and RawMaterialController REST endpoints

---
+++ back-end/src/main/java/com/autoflex/challenge/production_management_api/controller/ProductController.java
@@ -0,0 +1,59 @@
+package com.autoflex.challenge.production_management_api.controller;
+
+import com.autoflex.challenge.production_management_api.dto.request.ProductRequestDTO;
+import com.autoflex.challenge.production_management_api.service.ProductService;
+import io.swagger.v3.oas.annotations.Parameter;
+import io.swagger.v3.oas.annotations.responses.ApiResponse;
+import io.swagger.v3.oas.annotations.responses.ApiResponses;
+import jakarta.persistence.EntityNotFoundException;
+import jakarta.validation.Valid;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.validation.BindingResult;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
+
+@RestController
+@RequestMapping("/api/products")
+public class ProductController {
+
+    private final ProductService productService;
+
+    public ProductController(ProductService productService) {
+        this.productService = productService;
+    }
+
+    @GetMapping
+    public ResponseEntity<?> getAllProducts() {
+        return ResponseEntity.status(HttpStatus.OK).body(productService.getAllProducts());
+    }
+
+    @GetMapping("/{id}")
+    public ResponseEntity<?> getProductById(@PathVariable Long id) {
+        return ResponseEntity.status(HttpStatus.OK).body(productService.getProduct(id));
+    }
+
+    @GetMapping("/suggestions")
+    public ResponseEntity<?> getProductSuggestions() {
+        return ResponseEntity.status(HttpStatus.OK).body(productService.getProductSuggestion());
+    }
+
+    @PostMapping
+    public ResponseEntity<?> createProduct(@Valid @RequestBody ProductRequestDTO productRequestDTO) {
+            return ResponseEntity.status(HttpStatus.CREATED).body(productService.createProduct(productRequestDTO));
+    }
+
+
+    @PutMapping
+    public ResponseEntity<?> updateProduct(@Valid @RequestBody ProductRequestDTO productRequestDTO, @RequestParam Long id) {
+        return ResponseEntity.status(HttpStatus.OK).body(productService.updateProduct(id, productRequestDTO));
+    }
+
+    @DeleteMapping("/{id}")
+    public ResponseEntity<?> deleteProduct(@PathVariable Long id) {
+        productService.deleteProduct(id);
+        return ResponseEntity.status(HttpStatus.NO_CONTENT).build();
+    }
+
+}
---
+++ back-end/src/main/java/com/autoflex/challenge/production_management_api/controller/RawMaterialController.java
@@ -0,0 +1,47 @@
+package com.autoflex.challenge.production_management_api.controller;
+
+import com.autoflex.challenge.production_management_api.dto.request.RawMaterialRequestDTO;
+import com.autoflex.challenge.production_management_api.entity.RawMaterial;
+import com.autoflex.challenge.production_management_api.service.RawMaterialService;
+import jakarta.validation.Valid;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
+
+@RestController
+@RequestMapping("/api/raw-materials")
+public class RawMaterialController {
+
+    private final RawMaterialService rawMaterialService;
+    public RawMaterialController(RawMaterialService rawMaterialService) {
+        this.rawMaterialService = rawMaterialService;
+    }
+
+    @GetMapping
+    public ResponseEntity<?> getAllRawMaterials() {
+        return ResponseEntity.status(HttpStatus.OK).body(rawMaterialService.getAllRawMaterials());
+    }
+
+    @GetMapping("/{id}")
+    public ResponseEntity<?> getRawMaterialById(@PathVariable Long id) {
+        return ResponseEntity.status(HttpStatus.OK).body(rawMaterialService.getRawMaterialById(id));
+    }
+
+    @PostMapping
+    public ResponseEntity<?> createRawMaterial(@Valid @RequestBody RawMaterialRequestDTO rawMaterialRequestDTO) {
+        return ResponseEntity.status(HttpStatus.CREATED).body(rawMaterialService.createRawMaterial(rawMaterialRequestDTO));
+    }
+
+    @PutMapping
+    public ResponseEntity<?> updateRawMaterial(@Valid @RequestBody RawMaterialRequestDTO rawMaterialRequestDTO, @RequestParam Long id) {
+        return ResponseEntity.status(HttpStatus.OK).body(rawMaterialService.updateRawMaterial(rawMaterialRequestDTO, id));
+    }
+
+    @DeleteMapping("/{id}")
+    public ResponseEntity<?> deleteRawMaterial(@RequestParam Long id) {
+        rawMaterialService.deleteRawMaterial(id);
+        return ResponseEntity.status(HttpStatus.NO_CONTENT).build();
+    }
+}
