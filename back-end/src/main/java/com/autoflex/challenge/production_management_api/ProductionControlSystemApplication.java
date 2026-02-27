855342194fbb7d6fe03024299ca3f2c49bb81e2c
Henrique Corazza <henrique.corazza@hotmail.com>
2026-02-27 14:46:20 -0300

    chore: add application entry point and test structure

---
+++ back-end/src/main/java/com/autoflex/challenge/production_management_api/ProductionControlSystemApplication.java
@@ -0,0 +1,13 @@
+package com.autoflex.challenge.production_management_api;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ProductionControlSystemApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ProductionControlSystemApplication.class, args);
+	}
+
+}
---
+++ back-end/src/test/java/com/autoflex/challenge/production_management_api/ProductionControlSystemApplicationTests.java
@@ -0,0 +1,13 @@
+package com.autoflex.challenge.production_management_api;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ProductionControlSystemApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
